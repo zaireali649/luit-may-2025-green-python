93e1069a3b3037d1ffa89aad37b2129475713ce1
Zaire Ali <zaireali649@gmail.com>
2025-06-03 18:41:48 -0400

    Update list_buckets.py

--- list_buckets.py
+++ list_buckets.py
@@ -1,20 +1,11 @@
-#%%
-
-# list S3 Buckets
-
 import boto3
 
 s3 = boto3.client('s3')
 
-#%%
-
 response = s3.list_buckets()
 
-#%%
-
-buckets = response["Buckets"]
-
-#%%
+buckets = response['Buckets']
 
 for bucket in buckets:
-    print(bucket["Name"])
+    if "Name" in bucket:
+        print(bucket['Name'])
