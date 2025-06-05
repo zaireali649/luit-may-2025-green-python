3324aa00fa9457924646ccde9acff1593767f7a5
Zaire Ali <zaireali649@gmail.com>
2025-06-05 18:42:31 -0400

    Update list_bucketsin_lambda.py

--- list_bucketsin_lambda.py
+++ list_bucketsin_lambda.py
@@ -1,10 +1,8 @@
 import json
 import boto3  # AWS SDK for Python; used to interact with AWS services
 
-def lambda_handler(event, context):
-    s3 = boto3.client('s3')  # Create an S3 client using default AWS credentials and region
-
-    response = s3.list_buckets()  # Call the S3 API to list all buckets in the account
+def list_buckets(s3_client):
+    response = s3_client.list_buckets()  # Call the S3 API to list all buckets in the account
 
     buckets = response['Buckets']  # Extract the list of buckets from the response
 
@@ -12,8 +10,17 @@ def lambda_handler(event, context):
 
     for bucket in buckets:  # Loop through each bucket in the list
         if "Name" in bucket:  # Check if the 'Name' key exists in the bucket dictionary
-            print(bucket['Name'])  # Print the name of the bucket
             bucket_names.append(bucket['Name'])  # Add the bucket name to the list
+
+    return bucket_names
+
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')  # Create an S3 client using default AWS credentials and region
+
+    bucket_names = list_buckets(s3)
+
+    for bucket_name in bucket_names:
+        print(bucket_name)  # Print the name of the bucket
 
     return {
         'statusCode': 200,  # Return a successful HTTP status code
